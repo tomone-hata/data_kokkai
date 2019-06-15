a7683477c46162389f8a34087ff0175ccc53545d
tomone-hata <tomone_hata@hi-labo.com>
2019-06-15 17:04:49 +0900

    First commit

---
+++ models.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import RMSprop
+from keras.utils import np_utils
+
+class Simplenn(object):
+    def __init__(self, x_dim, n_classes, hidden_size=[32,32], batch_size=32, \
+                 learning_rate=0.01, epochs=10, random_seed=None, class_weight=None, dropuout=0):
+        model = keras.models.Sequential()
+        self.x_dim = x_dim
+        self.n_classes = n_classes
+        self.batch_size = batch_size
+        self.hidden_size = hidden_size
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.random_seed = random_seed
+        self.class_weight = class_weight
+        self.dropuout = dropuout
+        self.model = Sequential()
+        print('Start to building a neural network.')
+        self.build()
+
+    def build(self):
+        self.model.add(keras.layers.Dense(units=self.hidden_size[0], \
+                                     input_dim=self.x_dim,
+                                     kernel_initializer='glorot_uniform', \
+                                     bias_initializer='zeros', \
+                                     activation='relu' \
+                                            ))
+        self.model.add(Dropout(self.dropuout))
+        self.model.add(keras.layers.Dense(units=self.hidden_size[1], \
+                                     input_dim=self.hidden_size[0],
+                                     kernel_initializer='glorot_uniform', \
+                                     bias_initializer='zeros', \
+                                     activation='relu' \
+                                            ))
+        self.model.add(Dropout(self.dropuout))
+        self.model.add(keras.layers.Dense(units=self.n_classes, \
+                                     input_dim=self.hidden_size[1],
+                                     kernel_initializer='glorot_uniform', \
+                                     bias_initializer='zeros', \
+                                     activation='softmax' \
+                                            ))
+        #sgd_optimizer = keras.optimizers.SGD(lr=self.learning_rate)
+        self.model.compile(loss='binary_crossentropy',
+                      optimizer=RMSprop(lr=self.learning_rate),
+                      metrics=['accuracy'])
+        print('Scucceeded in building a neural network.')
+
+
+    def train(self, x_train, y_train, x_valid, y_valid, one_hot=False):
+        print('Start to train.')
+        if one_hot:
+            y_train = np_utils.to_categorical(y_train)
+            y_valid = np_utils.to_categorical(y_valid)
+
+        self.model.fit(x_train, y_train, \
+                       batch_size=self.batch_size, \
+                       epochs=self.epochs, \
+                       verbose=1, \
+                       validation_data=(x_valid, y_valid),\
+                       class_weight = self.class_weight)
+        print('Scucceeded in training.')
+
+
+    def test(self, x_test, y_test, one_hot=False):
+        if one_hot:
+            y_test = np_utils.to_categorical(y_test)
+
+        score = self.model.evaluate(x_test, y_test, verbose=1)
+        print('Test loss:', score[0])
+        print('Test accuracy:', score[1])
---
+++ utils.py
@@ -0,0 +1,111 @@
+import MeCab
+import sentencepiece
+import copy
+import sentencepiece as spm
+from tqdm import tqdm_notebook
+import numpy as np
+from collections import OrderedDict
+import mojimoji
+import re
+
+
+
+
+pattern_tapple = ((r'[一二三四五六七八九十〇]+号', r'一号'), \
+                  (r'平成[一二三四五六七八九十〇]+年', r'平成一年'), \
+                  (r'[一二三四五六七八九十〇]+月', r'一月'), \
+                  (r'(午[(前)|(後)])[一二三四五六七八九十〇]+時',r'午前一時'), \
+                  (r'[一二三四五六七八九十〇]+月[一二三四五六七八九十〇]+日', r'一月一日'),\
+                  (r'^[一二三四五六七八九十壱弐参拾百千万萬億兆〇]$', r'一'),\
+                  (r'^[0-9]+$', r'1'), \
+                  (r'^[0-9]+(|名|号|分|円|番|発|戦|年度|種|期|年|月|日|時|)$', r'1\1'), \
+                  (r'^[一二三四五六七八九十〇]+(|名|号|分|円|番|発|戦|年度|種|期|年|月|日|時|)$', r'1\1') \
+
+
+    )
+inflection_tapple = ('動詞', '形容詞', '助動詞')
+def translate_word(ma_word, stopwords = []):
+    word = ma_word[-3] if ma_word[1] in inflection_tapple else ma_word[0]
+    word = mojimoji.zen_to_han(word, kana=False)
+    #stopwordsが定義された場合の対応
+    if stopwords and word in stopwords:
+        word = ''
+        return word
+
+    if ma_word[2] == '固有名詞' and ma_word[3] == '人名':
+        word = '佐村河内守'
+        return word
+
+    for patterns in pattern_tapple:
+        word = re.sub(patterns[0], patterns[1], word)
+
+    return word
+
+
+def dalete_text_ma_column(df_train, df_test):
+    try:
+        df_train = df_train.drop('speech_text_ma', axis=1)
+        df_test  = df_test.drop('speech_text_ma', axis=1)
+    except KeyError:
+        pass
+
+    return df_train, df_test
+
+##########Morphological analysis with MeCab##########↓
+class Mecab_Analysis(object):
+    def __init__(self, dic_path=''):
+        try:
+            if dic_path:
+                dic_path = '-d %s' % dic_path
+                self.m = MeCab.Tagger(dic_path)
+            else:
+                self.m = MeCab.Tagger()
+        except RuntimeError:
+            print('Invalid dictionary path. Use the default dictionary.')
+            self.m = MeCab.Tagger()
+
+        self.m.parse('')
+
+
+    def Morphological_Analysis(self, text):
+        result_list = []
+        m_texts = self.m.parse(text)
+        m_texts = m_texts.split('\n')
+        for m_text in m_texts:
+            m_text = m_text.split('\t')
+            word = m_text[0]
+            if word == 'EOS':
+                break
+
+            pos = m_text[1]
+            pos = pos.split(',')
+            append_list = [word] + pos
+            result_list.append(copy.copy(append_list))
+
+        return result_list
+##########Morphological analysis with MeCab##########↑
+
+
+def create_chunk_dataset(df, chunk_size=5):
+    assert chunk_size % 2 == 1, 'chunk_size should be odd number.'
+    labels = df['committee'].values.tolist()
+    texts = df['speech_text_ma'].values.tolist()
+    chunk_mean = chunk_size // 2
+    df_len = len(df)
+    #先頭のラベルをdfと合わせるために先頭にchunk_meanの数だけ末尾のindexの要素を加える
+    texts = [texts[idx] for idx in range(chunk_mean*(-1), 0)] + \
+            texts + \
+            [texts[idx] for idx in range(0, chunk_mean+1)]
+    #先頭に加えた末尾を捨てる
+    result_texts = []
+    #真ん中のword頻度を最大に端に行けば行くほど頻度を少なくする
+    for idx in range(0, df_len):
+        words = []
+        for jdx in range(idx, idx+chunk_size):
+            origin_point = jdx - idx
+            word_freq = chunk_mean - abs(chunk_mean - origin_point)
+            words.extend([texts[jdx] for _ in range(0, word_freq+1)])
+
+        result_texts.append(' '.join(words))
+
+    return result_texts, labels
